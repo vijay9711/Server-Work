090ecdc1e8bf9c12526008b668bb4248be8e36d4
vijay9711 <vijaysm97@gmail.com>
2024-09-02 17:17:14 -0500

    first

---
+++ app.js
@@ -0,0 +1,14 @@
+// index.js
+
+require('dotenv').config(); 
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.get('/', (req, res) => {
+    res.send(`Hello, Vercel!, I'm ${process.env.NAME}`);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}  I'm ${process.env.NAME}`);
+});
