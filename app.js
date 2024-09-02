83d864b83662365967f39ebb299f9b7043cff91e
vijay9711 <vijaysm97@gmail.com>
2024-09-02 17:50:08 -0500

    cors added

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 // index.js
 
-require('dotenv').config(); 
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3001;
+require('dotenv').config();
 
+const express = require('express');
+const cors = require('cors'); // Import cors
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Use CORS middleware with options
+const corsOptions = {
+    origin: '*', // Allow all origins. For specific origins, use an array, e.g., ['http://example.com']
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Allow cookies to be sent
+    optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions)); // Apply CORS middleware with options
+
+// Define routes
 app.get('/', (req, res) => {
-    res.send(`Hello, Vercel cloud!, I'm ${process.env.NAME}.`);
+    res.send(`Hello, Vercel! I'm ${process.env.NAME}.`);
 });
 
+// Listen on the provided port by Vercel
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}  I'm ${process.env.NAME}`);
+    console.log(`Server is running on port ${port}. I'm ${process.env.NAME}`);
 });
